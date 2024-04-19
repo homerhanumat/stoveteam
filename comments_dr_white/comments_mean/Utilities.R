d3bc3678e4f3c16188ba0921da63b2d9c8f553bc
Homer White <homerhanumat@gmail.com>
2024-04-18 22:44:51 -0400

    comments for Rhys:  radio buttons bring inputs as text, not numbers, so you must convert them to numbers prior to computing with them; see other comments in comments_mean folder

---
+++ comments_dr_white/comments_mean/Utilities.R
@@ -0,0 +1,262 @@
+###################################################
+#  Utility Functions for Generating Pareto Values
+###################################################
+
+rpareto <- function(n,alpha,theta) {#random values for Pareto(alpha,theta) distribution
+  theta*((1-runif(n))^(-1/alpha)-1)
+}
+
+dpareto <- function(x,alpha,theta) {  #pdf for Pareto(alpha,theta) distribution
+  alpha*theta^alpha/(x+theta)^(alpha+1)
+}
+
+
+############################
+# Generate the populations
+############################
+mu_norm <- 70
+sigma_norm <- 5
+shape_gamma <- 2
+scale_gamma <- 50
+
+# for pareto:
+alpha_pareto <- 5
+theta_pareto <- 100
+tail_prob <- 0.02  #want to find a Value at risk of 1 - this
+val_risk <- theta_pareto*(tail_prob^(-.5)-1)
+
+# for pop with group of outliers
+prop_outliers <- 0.10
+mean_outliers <- 200
+sd_outliers <- 5
+mean_regulars <- 50
+sd_regulars <- 5
+
+r_outlier<- function(n) {
+  prop_normals <- 1- prop_outliers
+  which_hump <- rbinom(n, size = 1, prob = prop_normals)
+  outlier_samp <- 
+    ifelse(
+      which_hump,
+      rnorm(n,mean=mean_regulars,sd=sd_regulars),
+      rnorm(n,mean=mean_outliers,sd=sd_outliers)
+    )
+  outlier_samp
+}
+
+d_outlier <- function(x) {
+  reg <- dnorm(x,mean=mean_regulars,sd=sd_regulars)
+  out <- dnorm(x,mean=mean_outliers,sd=sd_outliers)
+  (1-prop_outliers) * reg + prop_outliers * out
+}
+
+############################
+# Get the population means
+############################
+
+normal_mean <- mu_norm
+skew_mean <- shape_gamma*scale_gamma
+super_skew_mean <- theta_pareto/(alpha_pareto - 1)
+outlier_mean <- (1-prop_outliers)*mean_regulars+prop_outliers*mean_outliers
+
+###################
+# plot population
+###################
+
+draw_pop <- function(pop = c("normal", "skew", "super_skew", "outlier")) {
+  if (pop == "normal") {
+    df <- data.frame(
+      x = c(mu_norm - 3 * sigma_norm, mu_norm + 3 * sigma_norm)
+    )
+    p <-
+      ggplot(df, aes(x = x)) +
+      stat_function(
+        fun = dnorm,
+        n = 101,
+        args = list(
+          mean = mu_norm,
+          sd = sigma_norm
+        )
+      ) + 
+      labs(
+        x = "population value",
+        y = NULL
+      ) +
+      geom_vline(aes(xintercept = normal_mean))
+  }
+  if (pop == "skew") {
+    df <- data.frame(
+      x = c(0, shape_gamma*scale_gamma+7.5*sqrt(shape_gamma)*scale_gamma)
+    )
+    p <-
+      ggplot(df, aes(x = x)) +
+      stat_function(
+        fun = dgamma,
+        n = 101,
+        args = list(
+          shape = shape_gamma,
+          scale = scale_gamma
+        )
+      ) + 
+      labs(
+        x = "population value",
+        y = NULL
+      ) +
+      geom_vline(aes(xintercept = skew_mean))
+  }
+  if (pop == "super_skew") {
+    df <- data.frame(
+      x = c(0, val_risk)
+    )
+    p <-
+      ggplot(df, aes(x = x)) +
+      stat_function(
+        fun = dpareto,
+        n = 101,
+        args = list(
+          alpha = alpha_pareto,
+          theta = theta_pareto
+        )
+      ) + 
+      labs(
+        x = "population value",
+        y = NULL
+      ) +
+      geom_vline(aes(xintercept = super_skew_mean))
+  }
+  if (pop == "outlier") {
+    df <- data.frame(
+      x = c(0, mean_outliers +5 * sd_outliers)
+    )
+    p <-
+      ggplot(df, aes(x = x)) +
+      stat_function(
+        fun = d_outlier,
+        n = 201
+      ) + 
+      labs(
+        x = "population value",
+        y = NULL
+      ) +
+      geom_vline(aes(xintercept = outlier_mean))
+  }
+  p
+}
+
+#######################################
+# Function to get m samples of size n
+#######################################
+
+get_samples <- function(m, n, pop = c("normal", "skew", "super_skew", "outlier")) {
+  samps <- vector(mode = "list", length = m)
+  for (i in 1:m) {
+    if (pop == "normal") {
+      samps[[i]] <- rnorm(n, mean = normal_mean, sd = sigma_norm)
+    }
+    if (pop == "skew") {
+      samps[[i]] <- rgamma(n, shape = shape_gamma, scale = scale_gamma)
+    }
+    if (pop == "super_skew") {
+      samps[[i]] <- rpareto(n, alpha = alpha_pareto, theta = theta_pareto)
+    }
+    if (pop == "outlier") {
+      samps[[i]] <- r_outlier(n)
+    }
+  }
+  samps
+}
+
+##########################################
+# function to get intervals from samples
+##########################################
+
+get_intervals <- function(samps, pop = c("normal", "skew", "super_skew", "outlier"), level) {
+  if (pop == "normal") mu <- normal_mean
+  if (pop == "skew") mu <- skew_mean
+  if (pop == "super_skew") mu <- super_skew_mean
+  if (pop == "outlier") mu <- outlier_mean
+  m <- length(samps)
+  good <- logical(m)
+  xbar <- numeric(m)
+  lower <- numeric(m)
+  upper <- numeric(m)
+  number = 1:m
+  for (i in 1:m) {
+    xs <- samps[[i]]
+    n <- length(xs)
+    xbar[i] <- mean(xs)
+    crit <- qt((1 + level) / 2, df = n-1)
+    margin <- crit * sd(xs) / sqrt(n)
+    lower[i] <- xbar[i] - margin
+    upper[i] <- xbar[i] + margin
+    good[i] <- mu >= lower[i] & mu <= upper[i]
+  }
+  df <- data.frame(
+    number = number,
+    xbar = xbar,
+    lower = lower,
+    upper = upper,
+    good = good
+  )
+  df
+}
+
+
+##############################
+# function to plot intervals
+##############################
+
+## HSW:  revise this as needed so that it gives a pretty graph
+## for both 50 and 100 intervals:
+
+interval_plot <- function(data, pop = c("normal", "skew", "super_skew", "outlier")) {
+  if (pop == "normal") mu <- normal_mean
+  if (pop == "skew") mu <- skew_mean
+  if (pop == "super_skew") mu <- super_skew_mean
+  if (pop == "outlier") mu <- outlier_mean
+  plot <- ggplot(data) +
+    geom_vline(aes(xintercept = mu)) +
+    geom_segment(
+      aes(
+        x = lower,
+        xend = upper,
+        y = number,
+        yend = number,
+        color = good
+      )
+    ) +
+    geom_point(aes(x = xbar, y = number), size = 0.5) +
+    labs( x = NULL, y = NULL) +
+    theme(
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank()
+    )
+  plot
+}
+
+################################
+# Function to plot percentages
+################################
+
+perc_plotr <- function(pop, int_count){
+
+if (pop == "normal") mu <- normal_mean
+if (pop == "skew") mu <- skew_mean
+if (pop == "super_skew") mu <- super_skew_mean
+if (pop == "outlier") mu <- outlier_mean
+
+df <- data.frame(
+  percents = percs,
+  intervals_drawn = int_count
+)
+
+df %>% 
+  ggplot(aes(x="intervals_drawn", y="percents"))+
+  geompoint()+
+  geomline()+
+  geom_hline(yintercept = mu)
+
+}
+
---
+++ comments_dr_white/comments_mean/app.R
@@ -0,0 +1,192 @@
+library(tidyverse)
+library(shiny)
+library(bslib)
+library(bsicons)
+library(shinyjs)
+
+## Load utility functions
+source("Utilities.R")
+
+## HSW:  I suggest leaving out the emojis, and 
+## changing the color to something less "loud" (please, not orange!)
+
+ui <- fluidPage(useShinyjs(),
+                theme = bslib::bs_theme(primary = "orange"),
+                fluidRow(
+                  column(
+                    width = 3,
+                    selectInput(
+                      "pop",
+                      "Population Type",
+                      c("normal", "skew", "super_skew", "outlier")
+                    ),
+                    numericInput("n", "Sample Size", min = 2, value = 10),
+                    sliderInput(
+                      "level",
+                      "Confidence Level",
+                      min = 10,
+                      max = 99,
+                      value = 50,
+                      step = 1,
+                      post = "%"
+                    ),
+                    radioButtons("m", "Intervals to Draw", c(50,100)),
+                    actionButton("make_intervals", "Make Intervals"),
+                    hidden(actionButton("start_over", "Start Over"))
+                  ),
+                  column(width = 9, tabsetPanel(id="inTabset",
+                    tabPanel("Population Graph",
+                             ## HSW:  the above is not a good title.
+                             ## Why not:  "Population Graph"?
+                             ## make sure to revise your 
+                             ## updateTabsetPanel call accordingly
+                             fluidPage(
+                               fluidRow(plotOutput("cov_prop_plot")),
+                               fluidRow(
+                                 value_box(
+                                   "Population Mean",
+                                   textOutput("popMean"),
+                                   showcase = bsicons::bs_icon("emoji-heart-eyes"),
+                                   theme = "bg-gradient-green-teal",
+                                   height = "100%"
+                                   #showcase_layout = "bottom"
+                                 )
+                               )
+                             )),
+                    tabPanel("Confidence Intervals",
+                             fluidPage(
+                               fluidRow(plotOutput("more_ints_plot")),
+                               fluidRow(
+                                 column(
+                                   width = 4,
+                                   value_box(
+                                     "Intervals So Far",
+                                     textOutput("simCount"),
+                                     showcase = bsicons::bs_icon("emoji-heart-eyes"),
+                                     theme = "bg-gradient-green-teal",
+                                     height = "100%"
+                                     #showcase_layout = "bottom"
+                                   )
+                                 ),
+                                 column(
+                                   width = 4,
+                                   value_box(
+                                     "Number Containing Proportion",
+                                     textOutput("GI"),
+                                     showcase = bsicons::bs_icon("emoji-heart-eyes"),
+                                     theme = "bg-gradient-green-teal",
+                                     height = "100%"
+                                     #showcase_layout = "bottom"
+                                   )
+                                 ),
+                                 column(
+                                   width = 4,
+                                   value_box(
+                                     "Percentage Containing Proportion",
+                                     textOutput("percGI"),
+                                     showcase = bsicons::bs_icon("emoji-frown"),
+                                     theme = "bg-gradient-red-orange",
+                                     height = "100%"
+                                     #showcase_layout = "bottom"
+                                   )
+                                 )
+                               )
+                             ))
+                  ))
+                ))
+
+server <- function(input, output, session) {
+  
+  ###########################################################
+  # Function to keep track of intervals, means and coverage
+  ###########################################################
+  
+  rv <- reactiveValues(
+    ## HSW:  let's start off with zeros:
+    number = 0,
+    good = 0,
+    intervals = NULL,
+    ## HSW:  what's your plan with rv$percs?
+    percs = NULL
+  )
+  
+  ######################################
+  # Code to Control Element Visibility
+  ######################################
+  
+  observeEvent(input$make_intervals, {
+    hide("pop")
+    hide("n")
+    hide("level")
+    show("start_over")
+    ## HSW:  inputs form a radio button are of type
+    ## character, not numeric, so:
+    m <- as.numeric(input$m)
+    some_samps <- get_samples(m, input$n, input$pop)
+    ints_data <-
+      get_intervals(
+        some_samps,
+        input$pop,
+        input$level / 100
+      )
+    ## HSW:  change here too:
+    rv$number <- rv$number + m
+    rv$good <- rv$good + sum(ints_data$good)
+    rv$intervals <- ints_data
+## HSW:  you must always remove material like this when
+## resolving a merge conflict:
+## <<<<<<< HEAD
+ 
+
+    updateTabsetPanel(inputId = "inTabset",
+                      selected = "Confidence Intervals")
+## =======
+    
+
+    updateTabsetPanel(inputId = "inTabset",
+                      selected = "More Intervals at a Time")
+## >>>>>>> a05456eae6df945da34263df9deda8313d3685e6
+    
+  })
+  
+  observeEvent(input$start_over, {
+    show("pop")
+    show("n")
+    show("level")
+    hide("start_over")
+    rv$number <- 0
+    rv$good <- 0
+    rv$intervals<- NULL
+    rv$percs <- NULL
+    updateTabsetPanel(inputId = "inTabset",
+                      selected = "Population Graph")
+  })
+  
+  output$cov_prop_plot <- renderPlot({
+    draw_pop(input$pop)
+  })
+  
+  output$simCount <- renderText(rv$number)
+  output$GI <- renderText(sum(rv$good))
+  ## RCT: I added the as.numeric() because it said that these variables weren't numbers:
+  ## HSW:   That wasn't the issue:  we need sum of rv$good:
+  output$percGI <- renderText(round(sum(rv$good) / rv$number, 5) * 100)
+  
+  ## HSW:  see if you can fix this one:
+  ## I have a mean now but because it isn't reactive I believe it is the wrong one:
+  ## HSW: make this reactive on input$pop
+  ## use a switch statment (see R help on "switch") to set the mean to whatever
+  ## it should be
+  output$popMean <- renderText(mu_norm)
+  
+  # output$percs_plot <- 
+  #   renderPlot({
+  #     perc_plotr()
+  #   })
+  
+  output$more_ints_plot <-
+    renderPlot({
+      interval_plot(rv$intervals, input$pop)
+    })
+}
+shinyApp(ui, server)
